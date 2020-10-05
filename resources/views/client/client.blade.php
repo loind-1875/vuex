3e29fdad42f0e94f8910354d52280a7e0dee2ce1
nguyen.danh.loi <nguyen,danh.loi@sun-asterisk.com>
2020-10-05 15:01:34 +0700

    fix slider

--- resources/views/client/client.blade.php
+++ resources/views/client/client.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div class="slider-container rev_slider_wrapper" style="height: 670px;">
-        <div id="revolutionSlider" class="slider rev_slider" data-version="5.4.8" data-plugin-revolution-slider data-plugin-options="{'delay': 9000, 'gridwidth': 1170, 'gridheight': 670, 'disableProgressBar': 'on', 'responsiveLevels': [4096,1200,992,500], 'parallax': { 'type': 'scroll', 'origo': 'enterpoint', 'speed': 1000, 'levels': [2,3,4,5,6,7,8,9,12,50], 'disable_onmobile': 'on' }, 'navigation' : {'arrows': { 'enable': true }, 'bullets': {'enable': true, 'style': 'bullets-style-1', 'h_align': 'center', 'v_align': 'bottom', 'space': 7, 'v_offset': 70, 'h_offset': 0}}}">
+        <div id="revolutionSlider" class="slider rev_slider" data-version="5.4.8" data-plugin-revolution-slider data-plugin-options="{'delay': 9000, 'gridwidth': 1170, 'gridheight': 670, 'disableProgressBar': 'on', 'responsiveLevels': [4096,1200,992,500], 'parallax': { 'type': 'scroll', 'origo': 'enterpoint', 'speed': 10000, 'levels': [2,3,4,5,6,7,8,9,12,50], 'disable_onmobile': 'on' }, 'navigation' : {'arrows': { 'enable': true }, 'bullets': {'enable': true, 'style': 'bullets-style-1', 'h_align': 'center', 'v_align': 'bottom', 'space': 7, 'v_offset': 70, 'h_offset': 0}}}">
             <ul>
                 @forelse($sliders as $key => $slider)
                     <li class="slide-overlay" data-transition="fade">
@@ -17,19 +17,28 @@
                             data-bgrepeat="no-repeat"
                             class="rev-slidebg">
 
+                        <div class="tp-caption"
+                            data-x="center" data-hoffset="['0','0','0','0']"
+                            data-y="center" data-voffset="['-150','-150','-150','-150']"
+                            data-start="1000"
+                            data-transform_in="x:[-300%];opacity:0;s:500;"
+                            data-transform_idle="opacity:1;s:500;">
+                            <img src="/client/img/logo.png" alt="">
+                        </div>
+
                         <div class="tp-caption text-color-light font-weight-normal"
                             data-x="center"
                             data-y="center" data-voffset="['-50','-50','-50','-75']"
                             data-start="700"
                             data-fontsize="['16','16','16','40']"
                             data-lineheight="['25','25','25','45']"
-                            data-transform_in="y:[-50%];opacity:0;s:500;">{{ $slider->text1 }}</div>
+                            data-transform_in="y:[-50%];opacity:0;s:500;"></div>
 
                         <div class="tp-caption font-weight-extra-bold text-color-light negative-ls-1"
                             data-frames='[{"delay":1000,"speed":2000,"frame":"0","from":"sX:1.5;opacity:0;fb:20px;","to":"o:1;fb:0;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;fb:0;","ease":"Power3.easeInOut"}]'
                             data-x="center"
                             data-y="center"
-                            data-fontsize="['50','50','50','90']"
+                            data-fontsize="['22','22','22','40']"
                             data-lineheight="['55','55','55','95']">{{ $slider->text2 }}</div>
 
                         <div class="tp-caption font-weight-light ws-normal text-center text-color-light mt-4"
@@ -37,7 +46,7 @@
                             data-x="center"
                             data-y="center" data-voffset="['60','60','60','105']"
                             data-width="['530','530','530','1100']"
-                            data-fontsize="['18','18','18','40']"
+                            data-fontsize="['16','16','16','40']"
                             data-lineheight="['26','26','26','45']"
                             >
                             {{ $slider->text3 }}
